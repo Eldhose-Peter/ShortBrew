d1ff7889f32ba9123872dc29df7f5457fbf6f677
Eldhose-Peter <eldhosepeter2000@gmail.com>
2026-07-03 20:16:59 +0530

    initialised java backend

---
+++ backend/src/main/java/com/epproject/ShortBrew/ShortBrewApplication.java
@@ -0,0 +1,13 @@
+package com.epproject.ShortBrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortBrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortBrewApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/epproject/ShortBrew/ShortBrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epproject.ShortBrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortBrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
